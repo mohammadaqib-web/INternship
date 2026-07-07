761fe6615e937351e7f9a9b1d0125896ed26f4d1
mohammadaqib-web <mohdaqib918@gmail.com>
2026-07-07 11:07:04 +0530

    com

---
+++ animation/script.js
@@ -0,0 +1,56 @@
+let a = {
+  one: 1,
+};
+
+let b = {
+  one: 1,
+};
+
+console.log(a==b);
+
+// // let a = 76;
+
+// // if (a >= 70) {
+// //   console.log("Grade A");
+// // } else if (a >= 90) {
+// //   console.log("Grade B");
+// // } else if (a >= 50) {
+// //   console.log("Grade C");
+// // } else {
+// //   console.log("Fail");
+// // }
+
+// let day = 9;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+
+//   case 4:
+//     console.log("Thursday");
+//     break;
+
+//   case 5:
+//     console.log("Friday");
+//     break;
+
+//   case 6:
+//     console.log("Saturday");
+//     break;
+
+//   case 7:
+//     console.log("Sunday");
+//     break;
+
+//   default:
+//     console.log("Invalid day");
+// }
---
+++ javascript/script.js
@@ -0,0 +1,176 @@
+// const name = "Aqib";
+// const age = 20;
+// const city = "Lucknow";
+// const IsStudent = true;
+
+// console.log("Hello, My name is", name);
+// console.log(`Hello, My name is ${name}`);
+
+// const num1 = 10
+// const num2 = 20
+
+// console.log("Division = ", num1/num2);
+// console.log("Modulus = ", num1%num2);
+
+// let num = "150";
+
+// console.log(Number(num));
+// console.log(Boolean(num));
+// console.log(String(num));
+
+// let age = -60;
+
+// if (age >= 1 && age < 13) {
+//   console.log("Child");
+// } else if (age >= 13 && age <= 17) {
+//   console.log("Teenager");
+// } else if (age >= 18 && age <= 59) {
+//   console.log("Adult");
+// } else if (age >= 60) {
+//   console.log("Senior Citizen");
+// } else {
+//   console.log("Negative values are not allowed");
+// }
+
+// let num = 231;
+
+// if (num % 2 === 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+// let num = "0";
+
+// if (num > 0) {
+//   console.log("Positive");
+// } else if (num < 0) {
+//   console.log("Negative");
+// } else {
+//   console.log("Zero");
+// }
+
+// let num1 = 10;
+// let num2 = 11;
+
+// if (num1 > num2) {
+//   console.log(`${num1} is greater`);
+// } else if (num2 > num1) {
+//   console.log(num2, " is greater");
+// } else {
+//   console.log("Both numbers are equal");
+// }
+
+// let password = "";
+
+// if (password.length >= 8) {
+//   console.log("Strong Password");
+// } else if (password.length) {
+//   console.log("Weak Password");
+// } else {
+//   console.log("Enter password");
+// }
+
+// let marks = 78;
+
+// if (marks >= 90 && marks <= 100) {
+//   console.log("A");
+// } else if (marks >= 80 && marks <= 89) {
+//   console.log("B");
+// } else if (marks >= 70 && marks <= 79) {
+//   console.log("C");
+// } else if (marks >= 60 && marks <= 69) {
+//   console.log("D");
+// } else if (marks < 60) {
+//   console.log("Fail");
+// }
+
+// let username = "";
+// let password = "1234@";
+
+// if (!username || !password) {
+//   console.log("Fields must not be empty");
+// } else if (username == "Admin" && password == "1234@") {
+//   console.log("Login Successful");
+// } else {
+//   console.log("Invalid username or password");
+// }
+
+// let signal = "red";
+
+// if (signal == "red") {
+//   console.log("Stop");
+// } else if (signal == "yellow") {
+//   console.log("Ready");
+// } else if (signal == "green") {
+//   console.log("Go");
+// } else {
+//   console.log("Invalid Signal");
+// }
+
+// let age = 20;
+// let nationality = "Indin";
+
+// if (age >= 18 && nationality === "Indian") {
+//   console.log("Eligible");
+// } else {
+//   console.log("Not Eligible");
+// }
+
+// let bill = 2000;
+// let discount;
+// let final;
+
+// if (bill > 5000) {
+//   discount = (bill * 20) / 100;
+//   final = bill - discount;
+
+//   console.log("Bill = ", bill);
+//   console.log("Discount = ", discount);
+//   console.log("Final Bill = ", final);
+// } else if (bill > 3000) {
+//   discount = (bill * 15) / 100;
+//   final = bill - discount;
+
+//   console.log("Bill = ", bill);
+//   console.log("Discount = ", discount);
+//   console.log("Final Bill = ", final);
+// } else if (bill > 1000) {
+//   discount = (bill * 10) / 100;
+//   final = bill - discount;
+
+//   console.log("Bill = ", bill);
+//   console.log("Discount = ", discount);
+//   console.log("Final Bill = ", final);
+// } else {
+//   console.log("No Discount");
+// }
+
+// let Name = "John";
+// let Age = 19;
+// let Marks = 55;
+// let Course = "Science";
+
+// if (Age >= 17 && Marks >= 60) {
+//   switch (Course) {
+//     case "Science":
+//       console.log("Admission Approved");
+//       console.log("Selected Course: Science");
+//       break;
+
+//     case "Commerce":
+//       console.log("Admission Approved");
+//       console.log("Selected Course: Commerce");
+//       break;
+
+//     case "Arts":
+//       console.log("Admission Approved");
+//       console.log("Selected Course: Arts");
+//       break;
+
+//     default:
+//       console.log("Invalid Course");
+//   }
+// } else {
+//   console.log("Not Eligible");
+// }
